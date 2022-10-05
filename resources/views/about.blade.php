3f860f3f2e01d52c3e4ae0c2a529efea230c59fa
Fauzan Agra <fauzanagra1@gmail.com>
2022-10-05 15:08:29 +0700

    Fixed Belajar Laravel 8 with Web Unpas - Part 7

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,11 +7,12 @@
 <img src="image/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">   
 @endsection
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptatem, error amet dignissimos magnam alias reiciendis non, sapiente quaerat voluptates placeat cum incidunt. A, ut. Nam illo non corporis, illum expedita sapiente, odio porro recusandae nemo sed repellendus.</p><p>Veritatis at earum aliquam deserunt dolor? Perspiciatis suscipit quasi tempora voluptatibus esse, veniam, explicabo, eveniet alias repudiandae id consequuntur cumque autem totam consequatur ab ut! Et recusandae a omnis accusantium laboriosam, rem natus neque, at officia, possimus enim veniam earum.</p><p>Beatae doloremque recusandae nisi adipisci, quos quod quidem neque at. Quia, aliquam unde voluptatum iure ipsam ad modi? Expedita a minus dolor dolore natus tempore labore! Repellat blanditiis odio autem labore animi earum, totam nobis molestiae laudantium illum, doloremque cumque architecto tempora.</p>
+{{-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptatem, error amet dignissimos magnam alias reiciendis non, sapiente quaerat voluptates placeat cum incidunt. A, ut. Nam illo non corporis, illum expedita sapiente, odio porro recusandae nemo sed repellendus.</p><p>Veritatis at earum aliquam deserunt dolor? Perspiciatis suscipit quasi tempora voluptatibus esse, veniam, explicabo, eveniet alias repudiandae id consequuntur cumque autem totam consequatur ab ut! Et recusandae a omnis accusantium laboriosam, rem natus neque, at officia, possimus enim veniam earum.</p><p>Beatae doloremque recusandae nisi adipisci, quos quod quidem neque at. Quia, aliquam unde voluptatum iure ipsam ad modi? Expedita a minus dolor dolore natus tempore labore! Repellat blanditiis odio autem labore animi earum, totam nobis molestiae laudantium illum, doloremque cumque architecto tempora.</p> --}}
 
 
-Post::create([
-    'title' => 'Judul Pertama',
-    'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptatem, error amet dignissimos magnam alias reiciendis non, sapiente quaerat voluptates placeat cum incidunt.',
+{{-- Post::create([
+    'title' => 'Judul Ke Tiga',
+    'slug' => 'judul-ke-tiga',
+    'excerpt' => 'Lorem ipsum ke tiga',
     'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptatem, error amet dignissimos magnam alias reiciendis non, sapiente quaerat voluptates placeat cum incidunt. A, ut. Nam illo non corporis, illum expedita sapiente, odio porro recusandae nemo sed repellendus.</p><p>Veritatis at earum aliquam deserunt dolor? Perspiciatis suscipit quasi tempora voluptatibus esse, veniam, explicabo, eveniet alias repudiandae id consequuntur cumque autem totam consequatur ab ut! Et recusandae a omnis accusantium laboriosam, rem natus neque, at officia, possimus enim veniam earum.</p><p>Beatae doloremque recusandae nisi adipisci, quos quod quidem neque at. Quia, aliquam unde voluptatum iure ipsam ad modi? Expedita a minus dolor dolore natus tempore labore! Repellat blanditiis odio autem labore animi earum, totam nobis molestiae laudantium illum, doloremque cumque architecto tempora.</p>'
-])
+]) --}}
